8d3f8a269cb7b906b915b9b7d424f3d724c25b99
Ruchie Roell <dj_ruchie@yahoo.com.ph>
2025-09-26 23:57:13 +0200

    added chartjs-set-up.js

---
+++ src/lib/chartjs-setup.js
@@ -0,0 +1,6 @@
+import { Chart as ChartJS } from 'chart.js/auto';
+// If you use plugins, register them here, e.g.:
+// import annotationPlugin from 'chartjs-plugin-annotation';
+// ChartJS.register(annotationPlugin);
+
+export default ChartJS;
--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { HashRouter } from 'react-router-dom';
+import './lib/chartjs-setup';
 
 import App from './App.jsx';
 import { SupabaseProvider } from './contexts/SupabaseContext';
